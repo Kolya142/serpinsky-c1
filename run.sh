2bf9b3d058ceb9446d41b5285f2111adfc48f18a
Kolya <87275865+Kolya142@users.noreply.github.com>
2024-11-08 14:03:44 +0300

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
-curl https://github.com/Kolya142/serpinsky-c1/raw/refs/heads/main/serpinsky1 > serpinsky.elf
+curl https://github.com/Kolya142/serpinsky-c1/raw/refs/heads/main/serpinsky1 -o serpinsky.elf
 chmod +x serpinsky.elf
 ./serpinsky.elf
