5e242810cabb239abff96711f21468909efe5d0a
Kolya <87275865+Kolya142@users.noreply.github.com>
2024-11-08 14:54:43 +0300

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
-wget https://github.com/Kolya142/serpinsky-c1/raw/refs/heads/main/serpinsky1.c
+curl https://github.com/Kolya142/serpinsky-c1/raw/refs/heads/main/serpinsky1.c > serpinsky1.c
 gcc serpinsky1.c -o serpinsky1
 ./serpinsky1
