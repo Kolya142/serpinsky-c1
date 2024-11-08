da66f4cbd518aea5377eceb56b5a1634fc3f78e4
Kolya <87275865+Kolya142@users.noreply.github.com>
2024-11-08 14:56:01 +0300

    gfix

--- run.sh
+++ run.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
-curl https://github.com/Kolya142/serpinsky-c1/raw/refs/heads/main/serpinsky1.c > serpinsky1.c
+curl https://raw.githubusercontent.com/Kolya142/serpinsky-c1/refs/heads/main/serpinsky1.c > serpinsky1.c
 gcc serpinsky1.c -o serpinsky1
 ./serpinsky1
